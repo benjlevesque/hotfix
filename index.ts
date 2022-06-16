d1d97ff0dfd1c47f09768fb175789f19da98b990
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2022-06-16 13:43:15 +0300

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,47 @@
+import { exec, execSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { SemVer } from "semver";
+
+yargs(hideBin(process.argv))
+  .command(
+    "$0 <hashes..>",
+    "",
+    (y) =>
+      y
+        .positional("hashes", {
+          desc: "The sha1 of the commit to cherry-pick",
+          type: "string",
+          array: true,
+          demandOption: true,
+        })
+        .option("tag", {
+          type: "string",
+          demandOption: true,
+          desc: "The existing tag to apply the fix on",
+        }),
+    ({ tag, hashes }) => {
+      const currentBranch = execSync(
+        `git rev-parse --abbrev-ref HEAD`
+      ).toString();
+      const newTag = new SemVer(tag).inc("patch").format();
+      const branchName = `hotfix-${newTag}`;
+      try {
+        execSync(`git checkout ${tag}`);
+        execSync(`git checkout -b ${branchName}`);
+        for (const hash of hashes) {
+          execSync(`git cherry-pick ${hash}`);
+        }
+        execSync(`git push origin ${branchName}`);
+        execSync(`git tag ${newTag}`);
+        execSync(`git push --tags`);
+        execSync(`gh release create ${newTag} --generate-notes`);
+      } catch (e) {
+        console.error(e.toString());
+      } finally {
+        execSync(`git checkout ${currentBranch}`);
+        execSync(`git branch -D ${branchName}`);
+      }
+    }
+  )
+  .demandCommand().argv;
